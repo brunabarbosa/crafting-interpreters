549c7bce9fed0ac5eb6ceeeac4465158c116b902
Bruna de Oliveira Barbosa <bruna.barbosa@opus-software.com.br>
2024-11-03 09:58:19 -0300

    first commit

---
+++ src/main/java/com/craftinginterpreters/Scanner.java
@@ -0,0 +1,15 @@
+package com.craftinginterpreters;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Scanner {
+
+    public Scanner(String token) {
+
+    }
+
+    public List<Token> scanTokens() {
+        return new ArrayList<>();
+    }
+}
---
+++ src/main/java/com/craftinginterpreters/Token.java
@@ -0,0 +1,4 @@
+package com.craftinginterpreters;
+
+public class Token {
+}
